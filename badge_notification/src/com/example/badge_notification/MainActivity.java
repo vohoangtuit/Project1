a55ee19e5c41db6caad6a273147817666fe0d606
vohoangtuit <vohoangtu2401gmail.com>
2016-01-27 14:00:40 +0700

    Mian Activity

--- badge_notification/src/com/example/badge_notification/MainActivity.java
+++ badge_notification/src/com/example/badge_notification/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends Activity {
 
     private EditText numInput;
 	// vohoangtuit
+	// add nhompro3
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
